d5edb14c416e4dba6d10f7733ff862901d51a219
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2023-12-15 15:58:36 +0500

    Related Data added.

--- index.js
+++ index.js
@@ -16,17 +16,36 @@ const resolvers = {
             return db.reviews
         },
         review(_, args){
-            return db.reviews.find((review)=> review.id == args.id)
+            return db.reviews.find((review)=> review.id === args.id)
         },
         game(_, args){
-            return db.games.find((game)=> game.id == args.id)
+            return db.games.find((game)=> game.id === args.id)
         },
         author(_, args){
-            return db.authors.find((author)=> author.id == args.id)
+            return db.authors.find((author)=> author.id === args.id)
         }
-        
+    },
+    Game: {
+        reviews(parent){
+            return db.reviews.filter((r)=> r.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews(parent){
+            return db.reviews.filter((r)=> r.author_id === parent.id)
+        }
+    },
+    Review: {
+        author(parent){
+            return db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent){
+            return db.games.find((g) => g.id === parent.game_id)
+        }
     }
 }
+
+
 
 const server = new ApolloServer({
     typeDefs,
